e943af07e78e4ae890ee133de5d34fd608a9f14b
jullinas <jllinas@xatoxi.com>
2024-06-04 17:36:58 -0400

    style para el background de login

--- src/RegisterAndLoginForm.jsx
+++ src/RegisterAndLoginForm.jsx
@@ -2,6 +2,7 @@ import {useContext, useState} from "react";
 import axios from "axios";
 import {UserContext} from "./UserContext.jsx";
 import logo from '../src/assets/logos/LogoApp.png'
+import backgroun from "../src/assets/background/fondo.png";
 
 export default function RegisterAndLoginForm() {
   const [username, setUsername] = useState('');
@@ -16,34 +17,55 @@ export default function RegisterAndLoginForm() {
     setId(data.id);
   }
   return (
-    <div className="bg-[url('./src/assets/background/fondo.jpg')] h-screen flex items-center">
+    <div
+      style={{
+        backgroundImage: `url(${backgroun})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        minHeight: "100vh", // Ensure minimum height for all viewports
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       <form className="w-64 mx-auto mb-12" onSubmit={handleSubmit}>
-      <img src={logo} className='h-15 w-15' alt="no found" />
-        <input value={username}
-               onChange={ev => setUsername(ev.target.value)}
-               type="text" placeholder="username"
-               className="block w-full rounded-sm p-2 mb-2 border" />
-        <input value={password}
-               onChange={ev => setPassword(ev.target.value)}
-               type="password"
-               placeholder="password"
-               className="block w-full rounded-sm p-2 mb-2 border" />
+        <img src={logo} className="h-15 w-15" alt="no found" />
+        <input
+          value={username}
+          onChange={(ev) => setUsername(ev.target.value)}
+          type="text"
+          placeholder="username"
+          className="block w-full rounded-sm p-2 mb-2 border"
+        />
+        <input
+          value={password}
+          onChange={(ev) => setPassword(ev.target.value)}
+          type="password"
+          placeholder="password"
+          className="block w-full rounded-sm p-2 mb-2 border"
+        />
         <button className="bg-blue-500 text-white block w-full rounded-sm p-2">
-          {isLoginOrRegister === 'register' ? 'Register' : 'Login'}
+          {isLoginOrRegister === "register" ? "Register" : "Login"}
         </button>
         <div className="text-center mt-2">
-          {isLoginOrRegister === 'register' && (
+          {isLoginOrRegister === "register" && (
             <div>
               Already a member?
-              <button className="ml-1" onClick={() => setIsLoginOrRegister('login')}>
+              <button
+                className="ml-1"
+                onClick={() => setIsLoginOrRegister("login")}
+              >
                 Login here
               </button>
             </div>
           )}
-          {isLoginOrRegister === 'login' && (
+          {isLoginOrRegister === "login" && (
             <div>
               Dont have an account?
-              <button className="ml-1" onClick={() => setIsLoginOrRegister('register')}>
+              <button
+                className="ml-1"
+                onClick={() => setIsLoginOrRegister("register")}
+              >
                 Register
               </button>
             </div>
